c9886e95d40bddce569e1c8e543b52af76c65ba6
Tanner Quigley <TannerQuigley@me.com>
2018-07-25 11:26:25 -0600

    shell true

--- startup.py
+++ startup.py
@@ -1,18 +1,19 @@
-from subprocess import call
+from subprocess import call,check_call
+import os
 
 #Install ansible
-call("sudo apt-get update")
-call("sudo apt-get install software-properties-common")
-call("sudo apt-add-repository ppa:ansible/ansible")
-call("sudo apt-get update")
-call("sudo apt-get install ansible")
+call("(sudo apt-get update)", shell=True)
+call("(sudo apt-get install software-properties-common)", shell=True)
+call("(sudo apt-add-repository ppa:ansible/ansible)", shell=True)
+call("(sudo apt-get update)", shell=True)
+call("(sudo apt-get install ansible)", shell=True)
 
 # git stuff
-call("sudo apt-get install git")
-call("git clone https://github.com/scottmotte/ansible-pi.git")
+# call("sudo apt-get install git")
+call("(git clone https://github.com/scottmotte/ansible-pi.git)", shell=True)
 
-call("cd ansible-pi")
-call("cp hosts.example hosts")
+call("(cd ansible-pi)", shell=True)
+call("(cp hosts.example hosts)", shell=True)
 # call("cp wpa_supplicant.conf.example wpa_supplicant.conf")
 
-call("ansible-playbook playbook.yml -i hosts --ask-pass --sudo -c paramiko")
+call("(ansible-playbook playbook.yml -i hosts --ask-pass --sudo -c paramiko)", shell=True)
